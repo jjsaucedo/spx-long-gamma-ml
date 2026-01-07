965352d9ce0090460ab05f29a46d244774e23828
jjsaucedo <jjsaucedo@gmail.com>
2026-01-06 19:02:16 -0600

    Add build_straddle function for options strategy

--- src/strategy/trade_builder.py
+++ src/strategy/trade_builder.py
@@ -1,1 +1,8 @@
+def build_straddle(symbol, strike, expiration):
+    return {
+        "symbol": symbol,
+        "expiration": expiration,
+        "strike": strike,
+        "structure": "ATM_STRADDLE"
+    }
 
