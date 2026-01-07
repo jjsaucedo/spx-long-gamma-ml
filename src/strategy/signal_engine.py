370f208c4b83dda7c69fd965354aadc54908025b
jjsaucedo <jjsaucedo@gmail.com>
2026-01-06 19:01:57 -0600

    Add generate_signal function for trading signals

--- src/strategy/signal_engine.py
+++ src/strategy/signal_engine.py
@@ -1,1 +1,9 @@
 
+def generate_signal(model, features_df):
+    X = features_df[["iv_mean", "gamma_weighted"]].mean().to_frame().T
+    prob = model.predict_proba(X)[0][1]
+
+    return {
+        "long_gamma_probability": prob,
+        "trade": prob > 0.65
+    }
